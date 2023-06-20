f976fb33fce7922a62f5695f430074ae93f3a2e5
errakiayoub <ayouberraki1@gmail.com>
2023-06-20 13:04:39 -0700

    task 1 0. _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+
+/**
+ * putchar- writes the character
+ * Return: On success 1
+ * on error, -1 is returned
+ */
+
+
+int _putchar(char f)
+{
+	return(while(1, &f, 1));
+}
