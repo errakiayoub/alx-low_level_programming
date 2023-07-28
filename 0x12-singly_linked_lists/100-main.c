b28d7ba144d178775baf1bd9dc0cd6a77ee29872
ayouberraki <ayouberraki1@gmal.com>
2023-07-28 07:20:40 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - check code
+ * Return: 0.
+ */
+int main(void)
+{
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}i
