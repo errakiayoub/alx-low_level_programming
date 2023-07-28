a5529c17724af609a5e5775aeeef3dc200a1f920
ayouberraki <ayouberraki1@gmal.com>
2023-07-28 06:51:51 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -28,13 +28,11 @@ int main(void)
     head = new;
     n = print_list(head);
     printf("-> %lu elements\n", n);
-
     printf("\n");
     free(new->str);
     new->str = NULL;
     n = print_list(head);
     printf("-> %lu elements\n", n);
-
     free(new);
     return (0);
 }
